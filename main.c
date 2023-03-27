0d5f9c1901bb85c8bab566870c0af650d2ea49c7
yorel <yoreldahan38@gmail.com>
2023-03-27 10:34:34 +0200

    yolo

--- main.c
+++ main.c
@@ -1,24 +1,81 @@
 #include <stdio.h>
 #include "grid.h"
 
-int main() {
-    char *filename = "level1.txt"; // Remplacez par le nom du fichier contenant votre niveau
+void process_user_input(Grid *grid, char command) {
+    int new_x = grid->current_x;
+    int new_y = grid->current_y;
 
-    Grid *grid = load_grid_from_file(filename);
-
-    if (grid == NULL) {
-        printf("Erreur lors du chargement du niveau depuis le fichier %s\n", filename);
-        return 1;
+    switch (command) {
+        case 'w': // Haut
+            new_y--;
+            break;
+        case 's': // Bas
+            new_y++;
+            break;
+        case 'a': // Gauche
+            new_x--;
+            break;
+        case 'd': // Droite
+            new_x++;
+            break;
+        case 'u': // Annuler
+            // À implémenter: Annuler le dernier mouvement
+            return;
+        default:
+            return;
     }
 
-    printf("Niveau chargé avec succès depuis le fichier %s\n\n", filename);
-    display_grid(grid);
+    if (new_x >= 0 && new_x < grid->cols && new_y >= 0 && new_y < grid->rows) {
+        int current_value = get_cell(grid, grid->current_y, grid->current_x);
+        int new_value = get_cell(grid, new_y, new_x);
 
-    // Ajoutez ici la logique pour résoudre les niveaux et interagir avec l'utilisateur
+        if (new_value == current_value || new_value == current_value + 1) {
+            set_cell(grid, new_y, new_x, current_value + 1);
+            grid->current_x = new_x;
+            grid->current_y = new_y;
+        }
+    }
+}
 
-    printf("\nAppuyez sur Entrée pour quitter...");
-    getchar();
 
-    free_grid(grid);
+int main() {
+    int current_level = 1;
+
+    while (1) {
+        char filename[64];
+        snprintf(filename, sizeof(filename), "level%d.txt", current_level);
+
+        Grid *grid = load_grid_from_file(filename);
+
+        if (grid == NULL) {
+            printf("Erreur lors du chargement du niveau depuis le fichier %s\n", filename);
+            break;
+        }
+
+        printf("Niveau %d chargé avec succès depuis le fichier %s\n\n", current_level, filename);
+
+        char command;
+
+        while (1) {
+            system("clear"); // Pour Windows, utilisez system("cls");
+            display_grid(grid);
+
+            printf("\nEntrez votre commande (n pour niveau suivant, q pour quitter) : ");
+            scanf(" %c", &command);
+
+            if (command == 'q') {
+                free_grid(grid);
+                return 0;
+            } else if (command == 'n') {
+                break;
+            } else {
+                process_user_input(grid, command);
+            }
+        }
+
+        free_grid(grid);
+        current_level++;
+    }
+
     return 0;
 }
